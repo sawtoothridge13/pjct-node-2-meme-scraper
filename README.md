fee3c8fb7d44898aa25ab034de743d37f86a18b6
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-01 14:11:03 +0200

    Project successfull!

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Create a node.js app to scrape first 10 memes from a website and save them to a 
 - [x] Command line cheatsheet steps (setup for Node.js project)
 - [x] Set up a `.gitignore` with the `memes` folder
 - [x] Create a folder called `"memes"`
-- [] Access the website (send a request to the website / "fetching")
-- [ ] Search for the `section` in the string of HTML that comes back (in the "response") from the website
-- [ ] Search for the first 10 image sources in the `section`
-  - [ ] Two options
+- [x] Access the website (send a request to the website / "fetching")
+- [x] Search for the `section` in the string of HTML that comes back (in the "response") from the website
+- [x] Search for the first 10 image sources in the `section`
+  - [x] Two options
     - [ ] One option: `.jpg` - this is in the HTML code of the website
-    - [ ] Another option: `<img`
-  - [ ] Get the `src` URL strings from the `img`
+    - [x] Another option: `<img`
+  - [x] Get the `src` URL strings from the `img`
 - [ ] Add the first 10 image URL strings to an array
 - [ ] Loop over the first 10 image URLs and:
   - [ ] Create a file (named correctly) in the `memes` folder (eg. `01.jpg`, `02.jpg`, etc)
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+// Import dependencies
+
 import fs from 'node:fs';
-import axios from 'axios';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
 
 const path = './memes';
 
 fs.access(path, (error) => {
-  // To check if the given directory
-  // already exists or not
+  // Check whether or not the directory exists
   if (error) {
-    // If current directory does not exist
-    // then create it
+    // If the directory doesn't exist, create it
     fs.mkdir(path, (error) => {
       if (error) {
         console.log(error);
@@ -20,3 +21,57 @@ fs.access(path, (error) => {
     console.log('Given Directory already exists !!');
   }
 });
+
+// Send a request to website using fetch for html data
+fetch('https://memegen-link-examples-upleveled.netlify.app/')
+  .then((res) => res.text())
+  .then((body) => {
+    const $ = cheerio.load(body);
+
+    // Fetch first 10 meme images from img URL and save them:
+    for (let i = 0; i < 10; i++) {
+      const memeImage = $('img')[i].attribs.src;
+
+      fetch(memeImage).then((res) => {
+        // Save each image and rename to a destination path
+        const filename = (i + 1).toString().padStart(2, '0') + '.jpg';
+        const path = './memes/' + filename;
+
+        const dest = fs.createWriteStream(path);
+        res.body.pipe(dest);
+      });
+    }
+
+    console.log('Download successful');
+  });
+
+//* ******************Trial and Error Code*********************/
+
+/* const imgUrls = [];
+
+const html = await axios.get(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+
+const $ = cheerio.load(html.data); */
+
+/*
+const imgs = $('div');
+imgs.each(function () {
+  const image = $(this).find('img').attr('src');
+
+  imgUrls.push(image);
+}); */
+
+/* import { promises as fs } from 'fs';
+
+const downloadImage = async (url, path) => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  const arrayBuffer = await blob.arrayBuffer();
+  const buffer = buffer.from(arrayBuffer);
+  await fs.writeFile(path, buffer);
+};
+
+await downloadImage('https://sabe.io/images/saturn.png', './saturn.png'); */
+//* ************************************************************** */
